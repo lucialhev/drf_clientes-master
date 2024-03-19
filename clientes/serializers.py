3c393d7109ecaf2a2de9fa0605df7c3151006409
lucialhev <luciahevicente@gmail.com>
2024-03-19 13:44:20 -0300

    Validações iniciais dos campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"Deve ter 11 dígitos"})
+        
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Apenas letras nesse campo"})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"o RG deve ter 9 digitos"})
+        
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':"Deve ter 11 dígitos"})
+        
+        return data
+ 
+ 
