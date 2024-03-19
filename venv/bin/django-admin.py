188ed925caeccd1aae02662d3e001c0603366c26
lucialhev <luciahevicente@gmail.com>
2024-03-19 13:01:52 -0300

    Configurações iniciais

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lucialhev@comgap.intraer/PyAlura/drf_clientes-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
